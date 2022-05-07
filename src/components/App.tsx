3e1c6fce5db12ac19a63c66c44013d9a45290868
Muhammad Ijlal Nasir <ijlalfarooqi30@gmail.com>
2022-05-07 19:03:18 +0500

    add comments

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -14,6 +14,7 @@ function App() {
     useState<boolean>(false);
   const [selected, setSelected] = useState<any>(null);
 
+  // Search products
   const searchProducts = (term: string) => {
     if (!term) {
       setSearchedProducts(null);
@@ -29,6 +30,7 @@ function App() {
     setSearchedProducts(items);
   };
 
+  // Filter products by category
   const searchByCategory = (cat: string) => {
     if (!cat) {
       setSearchedProducts(null);
@@ -45,6 +47,7 @@ function App() {
     setSearchedProducts(items);
   };
 
+  // Toggle details view when item is clicked
   const handleOnClickSearchItem = (item: any) => {
     setSelected(item);
     setToggleProductDetails(true);
@@ -77,6 +80,7 @@ function App() {
             />
           </Row>
 
+          {/* Render only if there are search results */}
           {searchedProducts && (
             <Row className="mt-4 mb-5">
               {searchedProducts.map((item: Product, index: number) => (
@@ -91,6 +95,7 @@ function App() {
           )}
         </Col>
 
+        {/* Render only if there are search results and user clicked on searched item and there is a selected item */}
         {searchedProducts && toggleProductDetails && selected && (
           <Col md={4} className="mt-4">
             <ProductDetails item={selected} />
